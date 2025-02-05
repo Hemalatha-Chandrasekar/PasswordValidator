51d488ba9969417e8d24c9d18880575d61d2b7e1
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2025-02-05 09:39:18 -0600

    Initial Commit

--- src/main/java/com/hema/PasswordValidator.java
+++ src/main/java/com/hema/PasswordValidator.java
@@ -1,4 +1,9 @@
 package com.hema;
 
 public class PasswordValidator {
+
+    public static boolean isValidPassword(String password) {
+        return password.length() >= 8;
+    }
 }
+
--- src/test/java/com/hema/PasswordValidatorTest.java
+++ src/test/java/com/hema/PasswordValidatorTest.java
@@ -1,4 +1,30 @@
+package com.hema;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PasswordValidatorTest {
-  
+    @BeforeEach
+    void setUp() {
+        PasswordValidator validator = new PasswordValidator();
+    }
+    @Test
+    void testValidPassword() {
+
+       // PasswordValidator validator = new PasswordValidator();
+        boolean password=PasswordValidator.isValidPassword("12345678");
+        assertTrue(password);
+
+       // assertTrue(PasswordValidator.isValidPassword("12345678"));
+    }
+
+    @Test
+    void shortPassword() {
+        //PasswordValidator validator = new PasswordValidator();
+        boolean password=PasswordValidator.isValidPassword("12345");
+        assertTrue(password);
+        //assertTrue(PasswordValidator.isValidPassword("12345"));
+    }
 }
